9e90f4a74237cc51204343bbf1d665c3b5ed385b
oppai <abderrahim.laazizi09@gmail.com>
2023-07-17 08:36:44 -0700

    alpha-ALPHA

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alp = 'a';
+	char ALP = 'A';
+
+	while (alp <= 'z')
+	{
+		putchar(alp);
+		alp++;
+	}
+
+	while (ALP <= 'Z')
+	{
+		putchar(ALP);
+		ALP++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
