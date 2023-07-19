b177ded6ae85250f7cba6c523f4bbb99be9e1e22
oppai <abderrahim.laazizi09@gmail.com>
2023-07-19 06:09:45 -0700

    #

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,21 @@
-#include "main.h"
+#include"main.h"
 
 /**
  * print_alphabet - print all alphabet in lowercase
  *
  */
 
-void print_alphabet_x10(void);
+void print_alphabet_x10(void)
+
 {
+
+
 	int ch;
 
 	for (ch = 'a'; ch <= 'z'; ch++)
 		_putchar(ch);
 
 	_putchar('\n');
+
 }
+
