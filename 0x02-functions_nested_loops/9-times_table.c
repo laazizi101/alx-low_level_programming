bd98bc99b7856e9052a38ff76cfdb75cf4211df6
oppai <abderrahim.laazizi09@gmail.com>
2023-07-19 18:24:08 -0700

    add

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -16,15 +16,13 @@ void times_table(void)
 			if (j == 0)
 			{
 				_putchar(k + '0');
-			}
-			else if (k < 10 && j != 0)
+			} else if (k < 10 && j != 0)
 			{
 				_putchar(',');
 				_putchar(' ');
 				_putchar(' ');
 				_putchar(k + '0');
-			}
-			else if (k >= 10)
+			} if (k >= 10)
 			{
 				     _putchar(',');
 				     _putchar(' ');
