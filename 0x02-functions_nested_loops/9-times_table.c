08a9695eca96678989f79ebfbd6f23562b2f99c4
oppai <abderrahim.laazizi09@gmail.com>
2023-07-19 19:19:23 -0700

    9-table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,29 +6,23 @@
  */
 void times_table(void)
 {
-	int i, j, k;
+	int num, mult, prod;
 
-	for (i = 0; i < 10; i++)
+	for (num = 0; num <= 9; num++)
 	{
-		for (j = 0; j < 10; j++)
+		_putchar(48);
+		for (mult = 1; mult <= 9; mult++)
 		{
-			k = j * i;
-			if (j == 0)
-			{
-				_putchar(k + '0');
-			} else if (k < 10 && j != 0)
-			{
-				_putchar(',');
+			_putchar('.');
+			_putchar(' ');
+
+			prod = num * mult;
+
+			if (prod <= 9)
 				_putchar(' ');
-				_putchar(' ');
-				_putchar(k + '0');
-			} if (k >= 10)
-			{
-				     _putchar(',');
-				     _putchar(' ');
-				     _putchar((k / 10) + '0');
-				     _putchar((k % 10) + '0');
-			}
+			else
+				_putchar((prod / 10) + 48);
+			_putchar((prod % 10) + 48);
 		}
 		_putchar('\n');
 	}
