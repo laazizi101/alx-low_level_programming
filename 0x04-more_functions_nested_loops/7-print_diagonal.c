70be2e0b34e6346e54108065b002f66ad36d0c10
oppai <abderrahim.laazizi09@gmail.com>
2023-07-21 10:59:34 -0700

    #

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -7,17 +7,22 @@
 
 void print_diagonal(int n)
 {
-	int post, space;
-
 	if (n <= 0)
-		_putchar('\n');
-	else
 	{
-		for (post = 1; post <= n; post++)
+		_putchar('\n');
+	} else
+	{
+		int i, j;
+
+		for (i = 0; i < n; i++)
 		{
-			for (space = 1; space <= post; space++)
-				_putchar(' ');
-			_putchar('/');
+			for (j = 0; j < n; j++)
+			{
+				if (j == i)
+					_putchar('\\');
+				else if (j < i)
+					_putchar(' ');
+			}
 			_putchar('\n');
 		}
 	}
